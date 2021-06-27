2ca766aa4eff6fc0323449aa8865271297795e75
PRAKASH <PRAKASH@DESKTOP-T1LERNF>
2021-06-27 23:23:38 +0530

    Testing the code....

---
+++ Proj/src/com/nt/comp/Main.java
@@ -0,0 +1,14 @@
+package com.nt.comp;
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+	  Main m=new Main();
+	  A a1=new A();
+	  B b1=new B();
+	  System.out.println("Adding value of::"+a1.sum(30,20));
+	  System.out.println("Subtracting value of::"+b1.sub(30, 20));
+	}
+
+}
